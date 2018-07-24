f491c67edf86d44c6fbe529a6f77f7f97104c9ff
Jamie Nicol <jamie.nicol@mail.utoronto.ca>
2018-07-23 21:55:05 -0400

    Added ability to fire projectiles
    Projectiles will fire in the direction that the player is facing.

---
+++ Asteroids/Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+
+    public int bulletSpeed;
+
+    void Awake()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //rb2d.AddForce(transform.up * bulletSpeed);
+        rb2d.velocity = transform.up * bulletSpeed;
+    }
+}
