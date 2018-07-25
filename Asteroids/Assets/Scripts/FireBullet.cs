6a9cc8db61811b2ee66eb1ed4be26527c6e7f828
Jamie Nicol <jamie.nicol@mail.utoronto.ca>
2018-07-25 19:06:38 -0400

    Added Animation and Fire Rate
    Player ship is now animated with fire coming out of its exhaust. Player also now has a fire rate of 1 bullet per second.

--- Asteroids/Assets/Scripts/FireBullet.cs
+++ Asteroids/Assets/Scripts/FireBullet.cs
@@ -2,20 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Script is linked to the blaster attached to player prefab
 public class FireBullet : MonoBehaviour {
 
+    // The bullet that we are firing
     public GameObject bullet;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
+    // trigger_reset used to limit blaster to firing once per second
+    private bool trigger_reset = true;
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0))
+        // The player can hold the mouse button down or just click it once
+        // trigger_reset allows at most 1 bullet per second
+        if (Input.GetMouseButton(0) && trigger_reset)
         {
-            Instantiate(bullet, transform.position, transform.rotation);
+            fire_bullet();
+            // Coroutine will yield for 1 second before resetting trigger_reset
+            StartCoroutine(reset_trigger());
         }
+    }
+
+    // Creates a new bullet prefab and fires it in the direction that the player is facing
+    void fire_bullet()
+    {
+        Instantiate(bullet, transform.position, transform.rotation);
+        trigger_reset = false;
+    }
+
+    // wait for 1 second and then reset the trigger so that player can fire again
+    IEnumerator reset_trigger()
+    {
+        yield return new WaitForSeconds(1f);
+        trigger_reset = true;
     }
 }
--- Asteroids/Assets/Scripts/Player.cs
+++ Asteroids/Assets/Scripts/Player.cs
@@ -6,39 +6,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    // rotationSpeed controls the speed that the player rotates towards the mouse
     public float rotationSpeed;
+    // moveSpeed controls how fast the player moves towards the mouse
     public float moveSpeed;
 
-    // x value can not be larger that |clampX|
-    public static Player instance;
+    //public static Player instance;
 
-    // this function ensures that there is only ever one game controller
-    void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else if (instance != this)
-        {
-            Destroy(gameObject);
-        }
-    }
+    //// this function ensures that there is only ever one game controller
+    //void Awake()
+    //{
+    //    if (instance == null)
+    //    {
+    //        instance = this;
+    //    }
+    //    else if (instance != this)
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
 
     // called once per frame
     void Update()
     {
         // Controls player rotation towards the mouse
+
+        // Set direction to the mouse pointer on the screen
         Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        // calculate the angle to rotate
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        // rotate the player
         Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
 
 
         // Controls player movement towards the mouse
+
+        // set the target position (the location of the mouse)
         Vector3 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // z value is constant
         targetPos.z = transform.position.z;
+        // move the player towards the mouse at moveSpeed
         transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
     }
 }
