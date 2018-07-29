8f54c312a0e01f162fcc86cad6a036adf9b09463
Jamie Nicol <jamie.nicol@mail.utoronto.ca>
2018-07-29 17:38:55 -0400

    Added Meteor Death Animation
    Meteors now play a 2 sprite animation before they are destroy

--- Asteroids/Assets/Scripts/Small_Meteor.cs
+++ Asteroids/Assets/Scripts/Small_Meteor.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Small_Meteor : MonoBehaviour {
 
     EdgeCollider2D ec2d;
+    Animator death_animation;
 
 	// Use this for initialization
 	void Start () {
-        ec2d = gameObject.GetComponent<EdgeCollider2D>();
-	}
+        ec2d = GetComponent<EdgeCollider2D>();
+        death_animation = GetComponent<Animator>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,16 +21,30 @@ public class Small_Meteor : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // if the meteor hits the ground, increase player score and destroy the enemy
+        // if a bullet hits the meteor, destroy the meteor and the bullet
         if (other.gameObject.CompareTag("Bullet"))
         {
             // remove the bullet
             Destroy(other.gameObject);
             // disable the collider and then play the small_meteor die animation
             ec2d.enabled = false;
+            // play the death animation
+            death_animation.SetBool("Alive", false);
             // remove enemy from game
-            //Destroy(this.gameObject);
+            StartCoroutine(destroy_meteor());
         }
+        
+        // if the player hits the meteor, destroy the player
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+        }
+    }
+
+    IEnumerator destroy_meteor()
+    {
+        yield return new WaitForSeconds(0.3f);
+        Destroy(this.gameObject);
     }
 
 }
