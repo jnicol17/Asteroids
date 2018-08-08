dcb4761db4905d3bad59ff792362d2690c583688
Jamie Nicol <jamie.nicol@mail.utoronto.ca>
2018-08-08 18:10:35 -0400

    Added spawning mechanics to Large Meteors
    Will experiement tomorrow with enabling small meteor colliders a set time after they spawn. Currently they collide with each other when spawned by a large meteor, but a 0.2 second delay appears to be enough when they are spawned separately.

--- Asteroids/Assets/Scripts/Large_Meteor.cs
+++ Asteroids/Assets/Scripts/Large_Meteor.cs
@@ -11,8 +11,11 @@ public class Large_Meteor : MonoBehaviour
     private Rigidbody2D rb2d;
 
     public Sprite damageSprite;
+    public GameObject small_meteor;
 
     public int health;
+
+    private IEnumerator coroutine;
 
     // Use this for initialization
     void Awake()
@@ -21,7 +24,7 @@ public class Large_Meteor : MonoBehaviour
         death_animation = GetComponent<Animator>();
         spr = GetComponent<SpriteRenderer>();
         rb2d = gameObject.GetComponent<Rigidbody2D>();
-        rb2d.velocity = transform.up * 5;
+        //rb2d.velocity = transform.up * 5;
 
     }
 
@@ -50,6 +53,9 @@ public class Large_Meteor : MonoBehaviour
                 pc2d.enabled = false;
                 // play the death animation
                 death_animation.SetBool("Alive", false);
+                // spawn two smaller meteors
+                coroutine = Spawn(30);
+                StartCoroutine(coroutine);
                 // remove enemy from game
                 StartCoroutine(destroy_meteor());
             }
@@ -66,5 +72,22 @@ public class Large_Meteor : MonoBehaviour
         yield return new WaitForSeconds(0.3f);
         Destroy(this.gameObject);
     }
+
+    IEnumerator Spawn(int angle)
+    {
+        Quaternion meteor1_rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        Instantiate(small_meteor, transform.position, meteor1_rotation);
+        yield return new WaitForSecondsRealtime(0.2f);
+        Debug.Log("HI");
+        Quaternion meteor2_rotation = Quaternion.AngleAxis(360 - angle, Vector3.forward);
+        Instantiate(small_meteor, transform.position, meteor2_rotation);
+    }
+
+    //public void spawn(int angle)
+    //{
+    //    // spawn an enemy at a random rotation
+    //    Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    //    Instantiate(small_meteor, transform.position, rotation);
+    //}
 
 }
