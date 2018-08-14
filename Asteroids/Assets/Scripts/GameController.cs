8e74a008a12e9b86a72ef899ac0fbf2ebd90ba42
Jamie Nicol <jamie.nicol@mail.utoronto.ca>
2018-08-13 21:45:26 -0400

    Added West side Spawners

--- Asteroids/Assets/Scripts/GameController.cs
+++ Asteroids/Assets/Scripts/GameController.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameController : MonoBehaviour {
+
+    public Spawner[] east_side_spawners;
+    public Spawner[] west_side_spawners;
+    public Spawner[] north_side_spawners;
+    public Spawner[] south_side_spawners;
 
 	// Use this for initialization
 	void Start () {
