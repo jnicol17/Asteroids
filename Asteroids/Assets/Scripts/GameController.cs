8df20265646fa3a78da3718cc35f6adda83212cc
Jamie Nicol <jamie.nicol@mail.utoronto.ca>
2018-08-21 21:29:26 -0400

    Added Spawning to Game Controller

--- Asteroids/Assets/Scripts/GameController.cs
+++ Asteroids/Assets/Scripts/GameController.cs
@@ -29,10 +29,40 @@ public class GameController : MonoBehaviour {
         //{
         //    spawner.Spawn();
         //}
+        StartCoroutine(spawn_routine());
     }
 
     // Update is called once per frame
     void Update () {
 
+    }
+
+    int getRandomNum(int min, int max)
+    {
+        return Random.Range(min, max);
+    }
+
+    void spawn(char nsew, int index)
+    {
+        if (nsew == 'n')
+            north_side_spawners[index].Spawn();
+        else if (nsew == 's')
+            south_side_spawners[index].Spawn();
+        else if (nsew == 'e')
+            east_side_spawners[index].Spawn();
+        else if (nsew == 'w')
+            west_side_spawners[index].Spawn();
+        else
+            Debug.Log("SPAWN ERROR");
+    }
+
+    IEnumerator spawn_routine()
+    {
+        yield return new WaitForSeconds(1f);
+        spawn('n', getRandomNum(0, north_side_spawners.Length));
+        spawn('s', getRandomNum(0, south_side_spawners.Length));
+        spawn('e', getRandomNum(0, east_side_spawners.Length));
+        spawn('w', getRandomNum(0, west_side_spawners.Length));
+        StartCoroutine(spawn_routine());
     }
 }
